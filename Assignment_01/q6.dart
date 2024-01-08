159a631f4405cbc45caa7ae866b2c4205c6183af
Muhammad-Saleem91 <m.saleemyameen.91@gmail.com>
2024-01-08 12:26:55 +0500

    correction

--- Assignment_01/q6.dart
+++ Assignment_01/q6.dart
@@ -3,13 +3,13 @@ import 'dart:io';
 void main() {
   print("Enter any Alphabet: ");
   String alphabet = stdin.readLineSync()!;
-  alphabet = alphabet.toLowerCase();
+  String alpha = alphabet.toLowerCase();
 
-  if (alphabet[0] == "a" ||
-      alphabet[0] == "e" ||
-      alphabet[0] == "i" ||
-      alphabet[0] == "o" ||
-      alphabet[0] == "u") {
+  if (alpha[0] == "a" ||
+      alpha[0] == "e" ||
+      alpha[0] == "i" ||
+      alpha[0] == "o" ||
+      alpha[0] == "u") {
     print(alphabet[0] + " is a vowel..");
   } else {
     print(alphabet[0] + " is a consonant..");
